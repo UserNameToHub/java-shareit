35871b0e3a7968d0696f7e9221b82b9eca272f28
aleksandrgineyka <meinepostkam@gmail.com>
2023-08-13 18:24:16 +0300

    style: удалить неспользуемый импорт

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.web.PageableDefault;
 import ru.practicum.shareit.booking.entity.Booking;
 import ru.practicum.shareit.booking.enumeration.Status;
 import ru.practicum.shareit.common.repository.BaseRepository;
