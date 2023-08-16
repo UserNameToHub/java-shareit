c2586ea9cc58f061459f7a59c454842d514885fd
aleksandrgineyka <meinepostkam@gmail.com>
2023-08-17 02:46:35 +0300

    feat: добавить валидацию на null

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import javax.validation.constraints.Future;
 import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,8 +16,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class BookItemRequestDto {
 	private long itemId;
+
+	@NotNull
 	@FutureOrPresent
 	private LocalDateTime start;
+
+	@NotNull
 	@Future
 	private LocalDateTime end;
 }
