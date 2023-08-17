e2107cc24cca11a0b1f1d617a162b61009493d0d
aleksandrgineyka <meinepostkam@gmail.com>
2023-08-18 01:26:24 +0300

    feat: добавить конфигурационный класс refactor: изменить поля и конструктор классов

---
+++ gateway/src/main/java/ru/practicum/shareit/common/config/WebClientConfiguration.java
@@ -0,0 +1,59 @@
+package ru.practicum.shareit.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.shareit.booking.BookingClient;
+import ru.practicum.shareit.item.ItemClient;
+import ru.practicum.shareit.request.RequestClient;
+import ru.practicum.shareit.user.UserClient;
+
+@Configuration
+public class WebClientConfiguration {
+    @Value("${shareit-server.url}")
+    public String serverUrl;
+
+
+    @Bean
+    public UserClient userClient(RestTemplateBuilder builder) {
+        var restTemplate = builder
+                .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + "/users"))
+                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                .build();
+
+        return new UserClient(restTemplate);
+    }
+
+    @Bean
+    public RequestClient requestClient(RestTemplateBuilder builder) {
+        var restTemplate = builder
+                .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + "/requests"))
+                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                .build();
+
+        return new RequestClient(restTemplate);
+    }
+
+    @Bean
+    public ItemClient itemClient(RestTemplateBuilder builder) {
+        var restTemplate = builder
+                .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + "/requests"))
+                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                .build();
+
+        return new ItemClient(restTemplate);
+    }
+
+    @Bean
+    public BookingClient bookingClient(RestTemplateBuilder builder) {
+        var restTemplate = builder
+                .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + "/requests"))
+                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                .build();
+
+        return new BookingClient(restTemplate);
+    }
+}
--- gateway/src/main/java/ru/practicum/shareit/user/UserClient.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserClient.java
@@ -1,44 +1,32 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.stereotype.Component;
-import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.client.RestTemplate;
 import ru.practicum.shareit.common.client.BaseClient;
 import ru.practicum.shareit.user.dto.UserDto;
 
-@Component
 public class UserClient extends BaseClient {
-    private static final String API_PREFIX = "/users";
-
-    @Autowired
-    public UserClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
-        super(builder
-                .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
-                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
-                .build());
+    public UserClient(RestTemplate restTemplate) {
+        super(restTemplate);
     }
 
-    ResponseEntity<Object> getAll() {
+    public ResponseEntity<Object> getAll() {
         return get("");
     }
 
-    ResponseEntity<Object> getById(Long id) {
+    public ResponseEntity<Object> getById(Long id) {
         return get("/" + id);
     }
 
-    ResponseEntity<Object> create(UserDto user) {
+    public ResponseEntity<Object> create(UserDto user) {
         return post("", user);
     }
 
-    ResponseEntity<Object> edit(UserDto user, Long userId) {
+    public ResponseEntity<Object> edit(UserDto user, Long userId) {
         return patch("/" + userId, user);
     }
 
-    ResponseEntity<Object> delete(Long userId) {
+    public ResponseEntity<Object> delete(Long userId) {
         return delete("/" + userId);
     }
 }
