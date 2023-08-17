509358d626069705512d8573946c13a6b792996b
aleksandrgineyka <meinepostkam@gmail.com>
2023-08-17 03:28:58 +0300

    style: to delete unused import

--- server/src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ server/src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.user.repository;
 
-import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.common.repository.BaseRepository;
 import ru.practicum.shareit.user.entity.User;
 
